916370fe66692e7683cb57f76a285c82dc744046
adikusin <adityakrsingh.ece18@itbhu.ac.in>
2024-07-18 12:30:10 +0530

    Added routes

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { signup,login } from "../controllers/auth.js";
+
+const router  = express.Router();
+
+// for creating a user
+router.post("/signup",signup)
+
+//sign in user
+router.post("/login",login)
+
+// //google auth based
+// router.post("/login",)
+
+export default router;
